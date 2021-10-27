7c6b8318922ece8aaadaae7eb21276000122884a
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-10-26 22:34:33 -0300

    feat: config on styles global and fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
-import styles from '../../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
       <Head><title>ig.news</title></Head>
-      Ola mundo
-    </h1>
-
+      <h1>
+        Ola mundo
+      </h1>
+    </>
   )
 }
