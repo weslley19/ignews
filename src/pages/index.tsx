75a297526bb0e5ab132b29473c11840dae7d61bc
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-10-27 23:11:06 -0300

    feat: build page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
-      <Head><title>ig.news</title></Head>
-      <h1>
-        Ola mundo
-      </h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about he <span>React</span> world</h1>
+          <p>Get access to all publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
